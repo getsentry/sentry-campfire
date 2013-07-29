8b0546957143ccb9469684280066022b0dbfac22
David Cramer <dcramer@gmail.com>
2013-07-29 00:32:08 -0700

    use get_absolute_url

--- sentry_campfire/plugin.py
+++ sentry_campfire/plugin.py
@@ -4,14 +4,13 @@
     ~~~~~~~~~~~~~~~~~~~~~~
 
     This module enables Campfire notifications from Sentry
-    
+
     :copyright: (c) 2012 Mustafa Khattab
-    :license: BSD    
+    :license: BSD
 """
 from camplight import Request, Campfire
 from django import forms
 
-from sentry.conf import settings
 from sentry.plugins import Plugin
 
 import sentry_campfire
@@ -41,19 +40,18 @@ class CampfireNotification(Plugin):
     ('Bug Tracker', 'https://github.com/mkhattab/sentry-campfire/issues'),
     ('Source', 'https://github.com/mkhattab/sentry-campfire'),
     ]
-    
+
     project_conf_form = CampfireOptionsForm
 
-    
-    def is_configured(self, project, **kwargs):        
+
+    def is_configured(self, project, **kwargs):
         return all(self.get_option(k, project) for k in ('url', 'token', 'rooms'))
 
     def post_process(self, group, event, is_new, is_sample, **kwargs):
         if not is_new or not self.is_configured(event.project):
             return
-        
-        link = '%s/%s/group/%d/' % (settings.URL_PREFIX, group.project.slug,
-                                    group.id)
+
+        link = group.get_absolute_url()
         message = '[%s] %s (%s)' % (event.server_name, event.message, link)
 
         self.send_notification(event.project, message)
@@ -67,11 +65,11 @@ class CampfireNotification(Plugin):
 
         req = Request(url.strip('/'), token) # The appended slash causes a 404 error
         campfire = Campfire(req)
-        
+
         for r in rooms.split(','):
             if r:
                 room = campfire.room(r)
                 room.speak(message)
                 if play_sound:
                     room.play(sound)
-                    
+
