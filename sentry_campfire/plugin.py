2fc25706e143711724d18168197240437e9d35e9
Matt Robenolt <m@robenolt.com>
2014-05-15 10:25:09 -0700

    Get server_name from the tags instead

--- sentry_campfire/plugin.py
+++ sentry_campfire/plugin.py
@@ -51,7 +51,7 @@ class CampfireNotification(Plugin):
             return
 
         link = group.get_absolute_url()
-        message = '[%s] %s (%s)' % (event.server_name, event.error(), link)
+        message = '[%s] %s (%s)' % (dict(event.get_tags().get('server_name')), event.error(), link)
 
         self.send_notification(event.project, message)
 
